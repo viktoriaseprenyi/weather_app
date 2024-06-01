103c61bb64a8055d63bb88ce1e6a3165c4738d96
Viktoria <bukovszki.viktoria@gmail.com>
2024-06-01 20:39:47 +0200

    Creat WeatherCard and Search components base

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { Search } from "./components/Search";
+import { WeatherCard } from "./components/WeatherCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="w-full h-[32rem] flex  flex-col justify-center items-center mb-10 mt-28">
+        <Search/>
+      <WeatherCard/>
+      </div>
   );
 }
 
---
+++ src/components/WeatherCard.js
@@ -0,0 +1,14 @@
+export function WeatherCard(){
+return (
+    <div className="flex flex-col h-full w-80 bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100
+    ">
+        <h2>London</h2>
+        <div>
+        <p className="text-5xl">34°C</p>
+        <p className="text-sm">Humidity: 10%</p>
+        </div>
+        
+        
+    </div>
+)
+};
